ed59b9fb4657b376bb055bad78b35568d540d011
Max <max.shao@icloud.com>
2025-03-20 10:42:29 +0800

    1

---
+++ src/main/java/com/Tia105G1Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tia105G1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tia105G1Application.class, args);
+    }
+
+}
