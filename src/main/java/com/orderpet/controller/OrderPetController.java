ff3f9b57f5cc4a82cc76ed4e03f443c48d77e896
FengTing <paul22455370@gmail.com>
2025-03-25 21:24:06 +0800

    預約頁面js、css、html檔案內路經更正

--- src/main/java/com/orderpet/controller/OrderPetController.java
+++ src/main/java/com/orderpet/controller/OrderPetController.java
@@ -2,6 +2,7 @@ package com.orderpet.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.orderpet.model.OrderPetService;
 
@@ -10,4 +11,10 @@ public class OrderPetController {
 	
 	@Autowired
 	private OrderPetService orderPetService;
+	
+	@GetMapping("/appointment")
+	public String getAppointnetn() {
+		
+		return "/front-end/appointment";
+	}
 }
