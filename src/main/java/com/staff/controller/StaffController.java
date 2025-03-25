c6a8457d28c475a4e3df18b4f99df0bdb2fe1941
Max <max.shao@icloud.com>
2025-03-25 16:42:37 +0800

    add StaffController to do post

--- src/main/java/com/staff/controller/StaffController.java
+++ src/main/java/com/staff/controller/StaffController.java
@@ -1,8 +1,25 @@
 package com.staff.controller;
 
 
+import com.staff.model.StaffService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class StaffController {
+
+    @Autowired
+    private StaffService staffService;
+
+    @PostMapping("/updateStaffStatus")
+    public String updateStaffStatus(){
+
+
+
+
+        return "redirect:/admin/home/page";
+    }
+
 }
