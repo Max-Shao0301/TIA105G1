8050081aca0b441b06025fd6d73129399f9cf953
Max <max.shao@icloud.com>
2025-03-27 16:24:52 +0800

    修改joinus.js 新增applyDTO

--- src/main/java/com/apply/controller/ApplyController.java
+++ src/main/java/com/apply/controller/ApplyController.java
@@ -7,13 +7,11 @@ import com.apply.model.ApplyVO;
 import com.staff.model.StaffService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Collections;
 
 
 @Controller
