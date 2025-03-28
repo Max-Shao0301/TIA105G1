82a0957aef367346b5868a76df6b3126b1e6e12f
Max <max.shao@icloud.com>
2025-03-28 16:26:32 +0800

    修改 前台加入我們正式版，增加驗證機制

--- src/main/java/com/apply/controller/ApplyController.java
+++ src/main/java/com/apply/controller/ApplyController.java
@@ -1,11 +1,8 @@
 package com.apply.controller;
 
 
-import com.springbootmail.MailService;
 import com.apply.model.ApplyService;
 import com.apply.model.ApplyVO;
-import com.staff.model.StaffService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,11 +22,7 @@ public class ApplyController {
 
     @Autowired
     private ApplyService applyService;
-    @Autowired
-    private MailService mailService;
 
-    @Autowired
-    private StaffService staffService;
 
     @PostMapping("/updateResult")
     public String updateStatus(@RequestParam("applyId") Integer applyId, @RequestParam("results") Integer results) {
@@ -45,32 +38,23 @@ public class ApplyController {
     @GetMapping("/joinus")
     public String joinus(Model model) {
         model.addAttribute("ApplyVO", new ApplyVO());
-        return "/front-end/joinus";
+        return "front-end/joinus";
     }
 
     @PostMapping("/insert/apply")
-    public String insertApply(@Valid ApplyVO applyVO, BindingResult result, ModelMap model,
+    public String insertApply(ApplyVO applyVO, BindingResult result, ModelMap model,
                               @RequestParam("license") MultipartFile [] parts) throws IOException {
-        if(parts[0].isEmpty()) {
-            model.addAttribute("errorMessage", "請上傳駕照");
-        } else {
-            for (MultipartFile multipartFile : parts) {
-                byte[] licensePhoto = multipartFile.getBytes();
-                applyVO.setLicense(licensePhoto);
-            }
+        for (MultipartFile multipartFile : parts) {
+            byte[] licensePhoto = multipartFile.getBytes();
+            applyVO.setLicense(licensePhoto);
         }
-        if (result.hasErrors() || parts[0].isEmpty()) {
-            return "front-end/joinus";
-        }
+
 
 
         applyService.addApply(applyVO);
         List<ApplyVO> applyList = applyService.getAll();
         model.addAttribute("applyList", applyList);
-        return "index";
+        return "redirect:/";
     }
-
-
-
 
 }
